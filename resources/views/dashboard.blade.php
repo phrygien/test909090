0e1e1cef934bf4b6b3e4018fe0ced87dd0805ae5
Mecene <mecene@numherit.com>
2024-03-12 12:44:20 +0300

    setup tenancy

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,15 +1,29 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <h2 class="text-xl font-semibold leading-tight text-gray-800">
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                    <x-mc-modal title="TallStackUi" blur id="modal-id">
+                        <div class="space-y-4">
+                        <x-mc-input label="Name" icon="users" />
+                        <x-mc-input label="Prenom" icon="users" />
+                        <x-mc-pin prefix="M-" length="5" />
+                        <x-mc-input label="Email" icon="users" />
+                        </div>
+                        <x-mc-button class="mt-3" x-on:click="$modalClose('modal-id')">
+                            Close
+                        </x-mc-button>
+                    </x-mc-modal>
+
+                    <x-mc-button x-on:click="$modalOpen('modal-id')">
+                        Open
+                    </x-mc-button>
                 </div>
             </div>
         </div>
